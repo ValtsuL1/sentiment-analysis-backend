87812cda4662e5ce319d365ef6bcecf135d1438f
ValtsuL1 <linnavaltteri@gmail.com>
2025-03-28 14:37:49 +0200

    added sentiment analysis

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 analyzer = SentimentIntensityAnalyzer()
 
-@app.route("/")
+@app.route('/')
 def hello_world():
     return "<p>Hello world!!!</p>"
 
-@app.route("/sentiment")
+# Post request that gets text from request,
+# analyzes it and returns the result
+@app.route('/sentiment', methods = ['POST'])
+@cross_origin()
 def get_sentiment():
-    data = request.json
-    print(data)
+    req = request.json
+    result = analyzer.polarity_scores(req['text'])
+    return result
 
 if __name__ == "__main__":
     app.run()
