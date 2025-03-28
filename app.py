5d83246be4a5394ebff9e16eda3e92835f748524
ValtsuL1 <linnavaltteri@gmail.com>
2025-03-28 12:34:20 +0200

    started implementing analysis

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 app = Flask(__name__)
+
+analyzer = SentimentIntensityAnalyzer()
 
 @app.route("/")
 def hello_world():
     return "<p>Hello world!!!</p>"
+
+@app.route("/sentiment")
+def get_sentiment():
+    data = request.json
+    print(data)
 
 if __name__ == "__main__":
     app.run()
